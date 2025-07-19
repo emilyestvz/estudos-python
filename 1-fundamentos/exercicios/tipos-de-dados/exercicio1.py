b50948347f8c82fedf77f760da1f3ce8677d9133
Emily <emilys@genstudents.org>
2025-07-19 20:01:32 -0300

    first commit

---
+++ 1-fundamentos/exercicios/tipos-de-dados/exercicio1.py
@@ -0,0 +1,9 @@
+# Crie três variáveis com os seguintes valores: um número inteiro, um número decimal e uma string. Use `type()` para imprimir o tipo de cada uma.
+
+num_inteiro = 27
+num_decimal = 3.14
+texto = "Hello, world!"
+
+print(type(num_inteiro))  # Deve imprimir <class 'int'>
+print(type(num_decimal))  # <class 'float'>
+print(type(texto))        # <class 'str'>
