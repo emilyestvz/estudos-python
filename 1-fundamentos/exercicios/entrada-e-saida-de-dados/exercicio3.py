29c9496531f318e51bb5920471fc5eabf29ae45b
Emily <emilys@genstudents.org>
2025-07-19 20:49:03 -0300

    :sparkles: feat: Solução do Exercício 3  sobre Entrada e saída de dados

---
+++ 1-fundamentos/exercicios/entrada-e-saida-de-dados/exercicio3.py
@@ -0,0 +1,6 @@
+# Receba a idade do usuário e exiba quantos anos ele terá em 10 anos.
+
+idade = int(input('\nDigite a sua idade: '))
+idade_futura = idade + 10
+
+print(f'\nEm 10 anos você terá {idade_futura} anos. ✨')
