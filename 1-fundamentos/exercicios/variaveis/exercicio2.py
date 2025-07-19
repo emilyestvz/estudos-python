1b7e176a53e154f65f4655f045fe6d9e35fb2d75
Emily <emilys@genstudents.org>
2025-07-19 20:34:04 -0300

    :sparkles: feat: Solução do Exercício 2 sobre variáveis

---
+++ 1-fundamentos/exercicios/variaveis/exercicio2.py
@@ -0,0 +1,7 @@
+# Troque os valores de duas variáveis entre si (ex: `a = 10`, `b = 20` → `a = 20`, `b = 10`).
+
+x = 10
+y = 20
+x, y = y, x
+
+print(f'\nValores das variáveis trocadas: x = {x}, y = {y}')
