777a20e5b50db1abbdfb09fc890651d74418d9cd
Emily <emilys@genstudents.org>
2025-07-19 20:47:27 -0300

    :sparkles: feat: Solução do Exercício 2 sobre Entrada e saída de dados

---
+++ 1-fundamentos/exercicios/entrada-e-saida-de-dados/exercicio2.py
@@ -0,0 +1,8 @@
+# Solicite dois números e exiba a soma deles. formando em numero em numero inteiro
+
+numero1 = float(input('\nDigite o primeiro número: '))
+numero2 = float(input('Digite o segundo número: '))
+
+soma = numero1 + numero2
+
+print(f'\nA soma de {numero1} + {numero2} é igual a {soma:.0f}')
