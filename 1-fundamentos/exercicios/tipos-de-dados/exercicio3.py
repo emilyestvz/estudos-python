c3d4695d21f59ad5799e76709f98c7ad5f01152c
Emily <emilys@genstudents.org>
2025-07-19 20:17:14 -0300

    :sparkles: feat: Solução para o Exercício 3

---
+++ 1-fundamentos/exercicios/tipos-de-dados/exercicio3.py
@@ -0,0 +1,12 @@
+# Crie um dicionário chamado `pessoa` com as chaves `nome`, `idade` e `altura`. Atribua valores e imprima o conteúdo.
+
+pessoa = {
+    "nome": "Emily",
+    "idade": 27,
+    "altura": 1.67}
+
+print(f'''\n📚 Dicionário de Pessoa:
+      Nome: {pessoa["nome"]}
+      Idade: {pessoa["idade"]}
+      Altura: {pessoa["altura"]} m.
+      ''')
