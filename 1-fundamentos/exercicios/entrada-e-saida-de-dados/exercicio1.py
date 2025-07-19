4e96ed49ebdc1c98926c70cfa1eebe89bd55edc1
Emily <emilys@genstudents.org>
2025-07-19 20:40:32 -0300

    :sparkles: feat: Solução do Exercício 1  sobre Entradas e saída de dados

---
+++ 1-fundamentos/exercicios/entrada-e-saida-de-dados/exercicio1.py
@@ -0,0 +1,5 @@
+# Peça ao usuário que digite seu nome e exiba uma mensagem de boas-vindas com o nome.
+
+nome = input('\n📌 Digite o seu nome: ')
+
+print(f'\n🌟 Olá, {nome}! Seja bem-vindo(a) ao nosso programa! 🌟')
