54e1856b65ed86d68d820c83ad1ba3a806b637db
Emily <emilys@genstudents.org>
2025-07-19 20:11:57 -0300

    :sparkles: feat: Solução para o Exercício 2

---
+++ 1-fundamentos/exercicios/tipos-de-dados/exercicio2.py
@@ -0,0 +1,4 @@
+# Escreva um código que armazene `True` em uma variável e verifique seu tipo com `type()`.
+
+bool = True
+print(f'Verificando a variável: {type(bool)}')  # Deve imprimir <class 'bool'>
