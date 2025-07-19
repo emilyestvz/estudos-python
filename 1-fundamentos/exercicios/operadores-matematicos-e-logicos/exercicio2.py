e6dee47835fb5373d7a61fe1dadc704352ddde41
Emily <emilys@genstudents.org>
2025-07-19 20:56:31 -0300

    :sparkles: feat: Solução do Exercício 2 sobre Operadores matemáticos e Lógicos

---
+++ 1-fundamentos/exercicios/operadores-matematicos-e-logicos/exercicio2.py
@@ -0,0 +1,8 @@
+# Verifique se um número informado pelo usuário é par ou ímpar.
+numero1 = int(input('\nInforme um número: '))
+
+if numero1 % 2 == 0:
+    print(f'O número {numero1} é par.')
+else: 
+    print(f'O número {numero1} é ímpar.')
+    
