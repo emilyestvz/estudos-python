98933dd94ef78f18815962c979f023181f8858cd
Emily <emilys@genstudents.org>
2025-07-19 20:23:44 -0300

    :sparkles: feat: Solução do Exercício 1 sobre variáveis

---
+++ 1-fundamentos/exercicios/variaveis/exercicio1.py
@@ -0,0 +1,6 @@
+# Armazene seu nome, idade e cidade em três variáveis diferentes e imprima uma frase usando esses dados.
+nome = 'Emily'
+idade = 27
+cidade = 'São Paulo'
+
+print(f'\nOlá mundo! Me chamo {nome}, tenho {idade} anos e moro em {cidade}. ✨')
