9224be49ccb07e72498a3615a87dded2dfc6b82c
Emily <emilys@genstudents.org>
2025-07-21 20:40:04 -0300

    :boom: fix: Correção do exercício

--- 1-fundamentos/exercicios/estruturas-condicionais/exercicio3.py
+++ 1-fundamentos/exercicios/estruturas-condicionais/exercicio3.py
@@ -5,11 +5,11 @@ print(f'Olá, {nome}! ✨')
 
 idade = int(input(f'\nDigite a sua idade: '))
 
-if idade <= 12:
-    print(f'{nome} é uma criança 👶')
-elif 13 <= idade <= 17:
-    print(f'{nome} é adolescente 🧑')
-elif 18 <= idade <= 59:
-    print(f'{nome} é adulta 👨')
+if idade >= 60:
+    print(f'{nome} é um idoso 👴👵')
+elif idade >= 18:
+    print(f'{nome} é um adulto 👨‍🦳👩‍🦳')
+elif idade >= 13:
+    print(f'{nome} é um adolescente 👦👧')
 else:
-    print(f'{nome} é idosa 👴')
+    print(f'{nome} é uma criança 👶👧👦')
